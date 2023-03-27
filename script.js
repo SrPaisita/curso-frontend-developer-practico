75f4e7cd2aef292514b7f4931a318db74ac2288a
SrPaisita <29paisa@gmail.com>
2023-03-27 11:58:23 -0500

    Shops,Aside,Mobile,Desktop menus

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const menuHamicon = document.querySelector('.menu')
+const mobilemenu = document.querySelector('.mobile-menu')
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener("click", toggleDesktopMenu)
+menuHamicon.addEventListener("click", toggleMobileMenu)
+menuCarritoIcon.addEventListener("click", toglgleAsideMenu)
 
 function toggleDesktopMenu() {
+    const asideMenuOpen = aside.classList.contains("inactive")
+
+    if(!asideMenuOpen) {
+        aside.classList.add("inactive")
+    }
     desktopMenu.classList.toggle("inactive")
+}
+
+function toggleMobileMenu() {
+    const asideMenuOpen = aside.classList.contains("inactive")
+
+    if (!asideMenuOpen){
+        aside.classList.add("inactive")
+    }
+    mobilemenu.classList.toggle("inactive")
+}
+
+function toglgleAsideMenu() {
+    const mobileMenuOpen = mobilemenu.classList.contains("inactive")
+    const DesktopMenuOpen = desktopMenu.classList.contains("inactive")
+
+    if (!mobileMenuOpen) {
+        mobilemenu.classList.add("inactive")
+    }
+
+    if(!DesktopMenuOpen){
+        desktopMenu.classList.add("inactive")
+    }
+    aside.classList.toggle("inactive")
 }
